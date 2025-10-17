6d2c6d0c6ce7ae37109696564f94e3753cbf21b8
jean_ITE <camposjean4@gmail.com>
2025-10-17 20:10:29 -0300

    Usando padrão MVC configurações do Controller e do Model

---
+++ app/Http/Controllers/PokemonController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pokemon;
+
+class PokemonController extends Controller
+{
+    public function index() {
+        $pokemon = Pokemon::all();
+
+        return view (
+                    'pokemons.index',
+        compact('pokemon'));     
+    }
+}
